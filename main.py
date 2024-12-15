66f496c154a6570051ded8ed40f0f20b691e70b3
Harut Grigoryan <harut.grigoryan@4r-soft.com>
2024-12-15 12:32:35 +0400

    Get areas one by one

--- main.py
+++ main.py
@@ -3,43 +3,41 @@ import geopandas as gpd
 import pandas as pd
 
 # List of states to retrieve data for
-states = [
-    "California, USA",
-    "Texas, USA",
-    "Florida, USA",
+areas = [
+    "New York, New York, USA",
+    "LA, CA, USA",
+    "SF, CA, USA",
     # Add more states as needed
 ]
 
-# Initialize lists to hold DataFrames
-all_nodes = []
-all_edges = []
+
+def transform_string(original_string):
+    # Remove commas and replace spaces with underscores
+    result_string = original_string.replace(',', '').replace(' ', '_')
+    return result_string
+
 
 # Function to fetch and append data for each state
-def fetch_and_append_state_data(state_name):
-    print(f"Fetching data for {state_name}")
+def fetch_and_append_state_data(area_name):
+    print(f"Fetching data for {area_name}")
     try:
-        # Fetch the graph data for each state
-        graph = ox.graph_from_place(state_name, network_type='drive')
-        nodes, edges = ox.graph_to_gdfs(graph)
+        nodes = []
+        edges = []
         
-        # Append DataFrames to the lists
-        all_nodes.append(nodes)
-        all_edges.append(edges)
+        # Fetch the graph data for each state
+        graph = ox.graph_from_place(area_name, network_type='all')
+        nodes, edges = ox.graph_to_gdfs(graph)
 
-        print(f"Finished fetching data for {state_name}")
+        # Save combined data to CSV files
+        nodes.to_csv(transform_string(area_name) + '_nodes.csv', index=False)
+        edges.to_csv(transform_string(area_name) + '_edges.csv', index=False)
+
+        print(f"Finished fetching data for {area_name}")
     except Exception as e:
-        print(f"An error occurred while fetching data for {state_name}: {e}")
+        print(f"An error occurred while fetching data for {area_name}: {e}")
 
 # Iterate over the list of states and fetch data
-for state in states:
-    fetch_and_append_state_data(state)
+for area_name in areas:
+    fetch_and_append_state_data(area_name)
 
-# Concatenate all DataFrames into one
-combined_nodes = pd.concat(all_nodes, ignore_index=True)
-combined_edges = pd.concat(all_edges, ignore_index=True)
-
-# Save combined data to CSV files
-combined_nodes.to_csv('all_states_nodes.csv', index=False)
-combined_edges.to_csv('all_states_edges.csv', index=False)
-
-print("Data from all states saved to all_states_nodes.csv and all_states_edges.csv")
+print("Job completed")
