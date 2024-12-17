c113eb8f75d0d83ef9dfc538426bf1cdc16c3cc2
Harut Grigoryan <harut.grigoryan@4r-soft.com>
2024-12-17 15:40:10 +0400

    thread

--- main.py
+++ main.py
@@ -2,26 +2,27 @@ from BuchungWriter import BuchungWriter
 import osmnx as ox
 import geopandas as gpd
 import pandas as pd
+import threading
 
 import logging
 logger = logging.getLogger(__name__)
 
 # List of states to retrieve data for
 areas = [
-    "New York, New York, USA",
-    "LA, CA, USA",
-    # "SF, CA, USA",
-    "Chicago, IL, USA",
-    "Houston, TX, USA",
-    "Philadelphia[d], PA, USA",
-    "San Antonio, TX, USA",
-    "San Diego, CA, USA",
-    "Dallas, TX, USA",
-    "Jacksonville[e, FL, USA",
-    "Austin, TX, USA",
-    "Fort Worth, TX, USA",
-    "San Jose, CA, USA",
-    "Seattle, WA, USA",
+    # "New York, New York, USA",
+    # "LA, CA, USA",
+    "SF, CA, USA",
+    # "Chicago, IL, USA",
+    # "Houston, TX, USA",
+    # "Philadelphia[d], PA, USA",
+    # "San Antonio, TX, USA",
+    # "San Diego, CA, USA",
+    # "Dallas, TX, USA",
+    # "Jacksonville[e, FL, USA",
+    # "Austin, TX, USA",
+    # "Fort Worth, TX, USA",
+    # "San Jose, CA, USA",
+    # "Seattle, WA, USA",
     # Add more states as needed
 ]
 
@@ -56,6 +57,12 @@ def fetch_and_append_state_data(area_name):
     logger.info(f"{len(edges)}")
     return edges
 
+def open_new_thread(awq, data):
+    # awq = BuchungWriter(context='')
+    t = threading.Thread(target=awq.run, args=(data,))
+    t.start()
+    return t
+
 if __name__ == '__main__':
     logging.basicConfig(
         filename='log-main_fv.log', 
@@ -67,34 +74,31 @@ if __name__ == '__main__':
 
     # Iterate over the list of states and fetch data
     for area_name in areas:
-        data = fetch_and_append_state_data(area_name)#.to_geo_dict()['features']
-        # data = data.iloc[:500]
-        # logger.info(f'\n{data}')
-        # logger.info(f'osmid = {data.iloc[0]['osmid']}')
-        # logger.info(f'highway = {data.iloc[0]['highway']}')
-        # logger.info(f'maxspeed = {data.iloc[0]['maxspeed']}')
-        # logger.info(f'name = {data.iloc[0]['name']}')
-        # logger.info(f'oneway = {data.iloc[0]['oneway']}')
-        # logger.info(f'reversed = {data.iloc[0]['reversed']}')
-        # logger.info(f'length = {data.iloc[0]['length']}')
-        # logger.info(f'geometry = {data.iloc[0]['geometry']}')
-        # logger.info(f'lanes = {data.iloc[0]['lanes']}')
-        # logger.info(f'ref = {data.iloc[0]['ref']}')
-        # logger.info(f'access = {data.iloc[0]['access']}')
-        # logger.info(f'bridge = {data.iloc[0]['bridge']}')
-        # logger.info(f'tunnel = {data.iloc[0]['tunnel']}')
-        # logger.info(f'width = {data.iloc[0]['width']}')
-        # logger.info(f'junction = {data.iloc[0]['junction']}')
-        # awq = BuchungWriter(context='')
-        # print(awq.run(data.iloc[:50]))
+        data = fetch_and_append_state_data(area_name)
         logger.info(f'data length = {len(data)}')
-        x = 0
-        y = 0
+        i = 0
         awq = BuchungWriter(context='')
-        while y < len(data):
-            y += 50
-            print(awq.run(data.iloc[x:y]))
-            x += 50
-            logger.info('\n\n\n-------------------------------------------------------\n\n\n')
+        # while i < len(data):
+        #     print(awq.run(data.iloc[i:i+50]))
+        #     i += 50
+            
+        threads = []
+        while i < 5000:
+            t = open_new_thread(awq, data.iloc[i:i+50])
+            logger.info('thread open')
+            threads.append(t)
+            i += 50
+        
+        for t in threads:
+            t.join()
+            logger.info('thread close')
+        
+        # t1 = threading.Thread(target=awq.run, args=(data.iloc[:50],))
+        # t1 = open_new_thread(awq, data.iloc[:50])
+        # t2 = open_new_thread(awq, data.iloc[50:100])
+        # t3 = open_new_thread(awq, data.iloc[100:150])
+        # t1.join()
+        # t2.join()
+        # t3.join()
 
     logger.info("Job completed")
